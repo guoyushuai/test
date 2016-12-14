565c35e15e6a9ada9ed29955ad51d35e13b66786
guoyushuai <guo210192@163.com>
2016-12-15 00:04:07 +0800

    Cache With Guava

--- junit_log/src/main/java/com/gys/service/UserServiceWithGuava.java
+++ junit_log/src/main/java/com/gys/service/UserServiceWithGuava.java
@@ -1,7 +1,59 @@
 package com.gys.service;
 
-/**
- * Created by Gys on 2016/12/14.
- */
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.gys.dao.UserDao;
+import com.gys.entity.User;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
 public class UserServiceWithGuava {
+
+    private static LoadingCache<String,User> loadingCache = CacheBuilder
+            .newBuilder()
+            .maximumSize(100)//LRU淘汰机制
+            .expireAfterAccess(60, TimeUnit.SECONDS)
+            .build(new CacheLoader<String, User>() {//若缓存中没有则执行该方法，到数据库中进行查询
+                @Override
+                public User load(String key) throws Exception {
+                    UserDao userDao = new UserDao();
+                    return userDao.findById(Integer.valueOf(key));
+                }
+            });
+
+    /*public User findById(Integer id) {
+        User user = loadingCache.getUnchecked(id.toString());//key
+        return user;
+    }*/
+
+    //分界线`````````````
+
+    private UserDao userDao = new UserDao();
+
+    private static Cache<String,User> cache = CacheBuilder
+            .newBuilder()
+            .maximumSize(100)
+            .expireAfterAccess(10,TimeUnit.MINUTES)
+            .build();
+
+    public User findById(Integer id) {
+        User user = null;
+        try {
+            user = cache.get("user:" + id, new Callable<User>() {
+                @Override
+                public User call() throws Exception {
+                    return userDao.findById(id);
+                }
+            });
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+
 }
--- junit_log/src/test/java/com/gys/service/UserServiceWithGuavaTestCase.java
+++ junit_log/src/test/java/com/gys/service/UserServiceWithGuavaTestCase.java
@@ -1,8 +1,33 @@
+package com.gys.service;
+
+import com.gys.dao.UserDao;
+import com.gys.entity.User;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-/**
- * Created by Gys on 2016/12/14.
- */
 public class UserServiceWithGuavaTestCase {
+
+    //视频演示错误 此处应为UserServiceWithGuava而不是UserService
+    private UserServiceWithGuava userServiceWithGuava;
+
+    //针对创建对象比较复杂的类
+    @Before
+    public void setUp() throws Exception {
+        userServiceWithGuava = new UserServiceWithGuava();
+    }
+
+    @Test
+    public void findById() throws Exception {
+
+        User user = userServiceWithGuava.findById(7);
+
+        //以下方式二选一
+        System.out.println(user);
+
+        assertNotNull(user);
+
+    }
 
 }
