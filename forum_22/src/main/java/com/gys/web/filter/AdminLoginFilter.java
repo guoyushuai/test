f00f3bc212ac19ec2a056a5218a2af36f549260a
guoyushuai <guo210192@163.com>
2017-01-02 01:36:48 +0800

    添加管理系统登录过滤器

--- forum_22/src/main/java/com/gys/web/filter/AdminLoginFilter.java
+++ forum_22/src/main/java/com/gys/web/filter/AdminLoginFilter.java
@@ -1,15 +1,40 @@
 package com.gys.web.filter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class AdminLoginFilter extends AbstractFilter {
 
+    private List<String> adminUrlList = null;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+        String validateAdminUrl = filterConfig.getInitParameter("validateAdminUrl");
+        adminUrlList = Arrays.asList(validateAdminUrl.split(","));
+    }
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        String uri = request.getRequestURI();
+
+        if(uri.startsWith("/admin")) {
+            if(adminUrlList != null && adminUrlList.contains(uri)) {
+                if(request.getSession().getAttribute("current_admin") == null) {
+                    response.sendRedirect("/admin/login?redirect=" + uri);
+                } else {
+                    filterChain.doFilter(request,response);
+                }
+            } else {
+                filterChain.doFilter(request,response);
+            }
+        }
     }
 }
